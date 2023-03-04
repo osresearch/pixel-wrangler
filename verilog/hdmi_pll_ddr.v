a07d15a38b729e29851268b734bbaf6e8d8f6daf
Trammell Hudson <hudson@trmm.net>
2023-03-04 11:48:58 +0100

    pll: add fixed delay and suddenly all the channels look good

--- verilog/hdmi_pll_ddr.v
+++ verilog/hdmi_pll_ddr.v
@@ -19,6 +19,11 @@
 //.DYNAMICDELAY(7'h11)
 //input [7:0] delay
 
+// 125 MHz == 8 ns/clock
+// 16 * 150ps per delay == 2.4 ns
+// maximum phase delay is 1/4 clock
+// guessing at a value seems to work well?
+
 module hdmi_pll(
 	input  clock_in,
 	output clock_out,
@@ -26,12 +31,17 @@ module hdmi_pll(
 	input reset
 	);
 
+	// total guess that seems to work well
+	parameter DELAY = 4'd8;
+
 SB_PLL40_CORE #(
 		.FEEDBACK_PATH("SIMPLE"),
 		.DIVR(4'b0000),		// DIVR =  0
-		.DIVF(7'b0100111),	// DIVF = 39 for simple
+		.DIVF(7'd39),	// DIVF = 4 for non-simple
 		.DIVQ(3'b011),		// DIVQ =  3
-		.FILTER_RANGE(3'b010)	// FILTER_RANGE = 2
+		.FILTER_RANGE(3'b010),	// FILTER_RANGE = 2
+		.FDA_FEEDBACK(DELAY),
+		.DELAY_ADJUSTMENT_MODE_FEEDBACK("FIXED")
 	) uut (
 		.LOCK(locked),
 		.RESETB(~reset),
